c77aa1dce3d5b73f48b2e520e429a525a7ac009c
Clemapfel <clemens.cords@aol.com>
2020-02-12 16:38:52 +0100

    stashing

--- benchmark.cpp
+++ benchmark.cpp
@@ -3,9 +3,10 @@
 // Copyright (c) 2020 Clemens Cords. All rights reserved.
 //
 #include <vector>
-#include <stdlib.h>
+#include <sstream>
 
 #include <benchmark/benchmark.h>
+
 #include <seqan3/alphabet/concept.hpp>
 #include <seqan3/search/algorithm/search.hpp>
 #include <seqan3/search/fm_index/fm_index.hpp>
@@ -13,75 +14,80 @@
 #include "input_generator.hpp"
 #include "kmer_index.hpp"
 
-// register programmatically with registerbenchmark: https://github.com/google/benchmark#output-files
-
-/*
-should_use_DA
-k
-alphabet
-n_queries
-query_size
-query_size_distribution: half non n*k, half nk for average case
-n hits per query
-% of queries with 0 hits
- */
-
-// name format: <text_size, k, query_size, n_queries, no_hit_ratio, use_da>
-
-// input config in a single struct for convenience
-struct input_generator_config
+// benchmark arguments struct for readability
+struct benchmark_arguments
 {
-    input_generator_config(
-        size_t n_queries,                   // total number of queries
-        std::vector<size_t> query_lengths,  // possible query lengths
-        size_t text_length,                 // length of text
-        float no_hit_queries_ratio = 0)     // percentage of queries that will have no occurence in text
-            : _n_queries(n_queries),
-              _query_lengths(query_lengths),
-              _text_length(text_length),
-              _no_hit_queries_ratio(no_hit_queries_ratio)
+    const size_t _query_size;               // size of every query
+    const size_t _n_queries;                // number of queries generated
+    const size_t _text_size;                // length of text
+    const float _no_hit_queries_ratio;      // text containts ratio * n_queries of the queries, 0 hits for rest
+
+    // ctor
+    benchmark_arguments(
+        size_t query_size,
+        size_t n_queries,
+        size_t text_size,
+        float no_hit_queries_ratio = 0)
+        : _query_size(query_size),
+          _n_queries(n_queries),
+          _text_size(text_size),
+          _no_hit_queries_ratio(no_hit_queries_ratio)
     {
     }
 
-    const size_t _query_length;
-    const size_t _n_queries;
-    const size_t _text_length;
-    const float _no_hit_queries_ratio;
-
-    std::string name(size_t k, std::string alphabet_id, bool use_da)
+    // generate queries and text pseudo-randomly
+    template<seqan3::alphabet alphabet_t>
+    void generate_queries_and_text(
+        std::vector<std::vector<alphabet_t>* queries, // out
+        std::vector<alphabet_t> text,                 // out
+        bool reset_state = true)                      // if true, returns same queries and text each call
     {
-        return "todo";
+        if (reset_state)
+            input_generator<alphabet_t>::reset_state();
+
+        if (queries != nullptr)
+        {
+            queries->clear();
+            for (auto q : input_generator<alphabet_t>::generate_queries(config._n_queries, config._query_lengths))
+                queries->push_back(q);
+        }
+
+        if (text != nullptr)
+        {
+            text->clear();
+            for (auto c : input_generator<alphabet_t>::generate_text(config._text_length, *queries))
+                text->push_back(c);
+        }
+    }
+
+     // generate csv header
+    static std::string get_header()
+    {
+        return "alphabet,text_size,k,query_size,n_queries,no_hit_query_ratio,uses_direct_addressing,";
+    }
+
+    // generate benchmark name which forms first few columns of csv
+    std::string get_name(size_t k, std::string alphabet_id, bool use_da)
+    {
+        return
+           "\"" + alphabet_id + "\","
+           + std::to_string(_text_size) + ","
+           + std::to_string(k) + ","
+           + std::to_string(_query_size) + ","
+           + std::to_string(_n_queries )+ ","
+           + std::to_string(_no_hit_queries_ratio) + ","
+           + std::to_string(use_da) + ",";
     }
 };
 
-// fill queries and vector based on config
-template<seqan3::alphabet alphabet_t>
-void generate_queries_and_text(
-    input_generator_config config,                  // in
-    std::vector<std::vector<alphabet_t>>* queries,  // out
-    std::vector<alphabet_t>* text)                  // out
-{
-    input_generator<alphabet_t>::reset_state();
-
-    queries->clear();
-    for (auto q : input_generator<alphabet_t>::generate_queries(config._n_queries, config._query_lengths))
-        queries->push_back(q);
-
-    text->clear();
-    for (auto c : input_generator<alphabet_t>::generate_text(config._text_length, *queries))
-        text->push_back(c);
-}
-
-// benchmark: kmer_index exact search time
+// benchmark: kmer index exact search
 template<seqan3::alphabet alphabet_t, bool use_da, size_t k>
-static void kmer_search(benchmark::State& state, input_generator_config input)
+static void kmer_search(benchmark::State& state, benchmark_arguments input)
 {
-    //auto input = input_generator_config(10, std::vector<size_t>{5, 6, 7}, 1000, 0.f);
-
     // generate text and queries
     std::vector<std::vector<alphabet_t>> queries;
     std::vector<alphabet_t> text;
-    generate_queries_and_text(input, &queries, &text);
+    input.generate_queries_and_text(&queries, &text, true);
 
     kmer_index<alphabet_t, k, uint64_t, uint32_t, use_da> index{text};
 
@@ -94,52 +100,43 @@ static void kmer_search(benchmark::State& state, input_generator_config input)
     }
 }
 
-// benchmark: fm_index exact search time
-template<seqan3::alphabet alphabet_t>
-static void fm_search(benchmark::State& state, input_generator_config input)
+// benchmark: fm index exact search
+template<seqan3::alphabet alphabet_t, bool use_da, size_t k>
+static void fm_search(benchmark::State& state, benchmark_arguments input)
 {
-    // generate text and queries
     std::vector<std::vector<alphabet_t>> queries;
     std::vector<alphabet_t> text;
-    generate_queries_and_text(input, &queries, &text);
+    input.generate_queries_and_text(&queries, &text, true);
 
-    seqan3::fm_index index{text};
+    fm_index index{text};
 
-    // cycle through queries
     unsigned long long i = 0;
     for (auto _ : state)
     {
-        seqan3::search(index, queries.at(i));
-        i = i < queries.size() ? i + 1 : 0;
+        search(index, queries.at(i));
+        i = i < queries.size()-1 ? i + 1 : 0;
     }
 }
 
-// wrapper that programmatically registers all
-template<seqan3::alphabet alphabet_t, bool use_da, size_t... ks>
-struct register_benchmark
+// benchmark: kmer_index construction
+template<seqan3::alphabet alphabet_t, bool use_da, size_t k>
+static void kmer_construction(benchmark::State& state, benchmark_arguments input)
 {
-    static void kmer_exact_search(std::vector<input_generator_config> configs)
-    {
-        for (auto config : configs)
-        {
-            (benchmark::RegisterBenchmark(config.name().c_str(), &kmer_search<alphabet_t, use_da, ks>, config), ...);
-        }
-    }
+    std::vector<alphabet_t> text;
+    input.generate_queries_and_text(nullptr, &text, true);
 
-    const std::vector<size_t> _query_lengths;
-    const size_t _n_queries;
-    const size_t _text_length;
-    const float _no_hit_queries_ratio;
-};
+    for (auto _ : state)
+        benchmark::DoNotOptimize(kmer_index<alphabet_t, k, uint64_t, uint32_t, use_da> index{text});
 
+    // log memory used
+    state["memory_used"] = kmer_index<alphabet_t, k, uint64_t, uint32_t, use_da>::estimate
+}
+
+// main
 int main(int argc, char** argv) {
 
-    auto cfg = input_generator_config(10, std::vector<size_t>{5, 6, 7}, 1000, 0.f);
-    register_benchmark<seqan3::dna4, false, 4, 5>::kmer_exact_search({cfg});
-
-    benchmark::Initialize(&argc, argv);
-    benchmark::RunSpecifiedBenchmarks();
+    auto cfg = benchmark_arguments(100, 10, 1000, 0);
+    std::cout << cfg.get_name(5, "dna4", true);
 }
-
 
 
--- kmer_index.hpp
+++ kmer_index.hpp
@@ -316,6 +316,8 @@ class kmer_index
                 return confirmed_positions;
             }
         }
+
+        // estimate size
 };
 
 template<seqan3::alphabet alphabet_t, size_t... ks>
