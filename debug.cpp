6014b63cf0c9b395bc106395a3e810afe70b6f25
Clemapfel <clemens.cords@aol.com>
2020-02-25 17:22:40 +0100

    [STABLE] rolled back fix, now bug free

--- debug.cpp
+++ debug.cpp
@@ -16,27 +16,11 @@ using namespace seqan3;
 
 int main()
 {
+    auto text = input_generator<dna4>::generate_text(100000, {"ACGT"_dna4});
 
-    auto text = input_generator<dna15>::generate_sequence(1e7);
-    auto index = make_kmer_index<10, 11, 12, 13, 14, 15, 16, true>(text);
-
-    debug_stream << index.search("ACGT"_dna15);
-
-    /*
-    std::vector<std::vector<dna5>> queries;
-    for (size_t length : {21, 25, 26})
-        for (auto q : input_generator<dna4>::generate_queries(1, length))
-            queries.push_back(q);
-
-    auto text = input_generator<dna5>::generate_text(10000, queries);
-
-    auto index = make_kmer_index<21, 25, 26, true>(text);
+    auto index = make_kmer_index<15, 4, 14>(text);
     fm_index fm{text};
 
-    for (auto q : queries)
-    {
-        debug_stream << index.search(q) << " | ";
-        debug_stream << search(q, fm) << "\n";
-    }
-     */
+        debug_stream << index.search("ACGT"_dna4) << " | ";
+        debug_stream << search("ACGT"_dna4, fm) << "\n";
 }
