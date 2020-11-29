f81998059d8e723c6cd14e9ad63111776974ee7e
clem <clemens.cords@aol.com>
2020-11-29 17:21:43 +0100

    stashing

--- benchmarks/map_vs_map/plot.R
+++ benchmarks/map_vs_map/plot.R
@@ -13,7 +13,7 @@ plot = plot + geom_line(mapping=aes(size, real_time, color=name), data=data[data
 plot = plot + geom_line(mapping=aes(size, real_time, color=name), data=data[data$name=="boost_median",], size=2)
 plot = plot + geom_line(mapping=aes(size, real_time, color=name), data=data[data$name=="abseil_median",], size=2)
 plot = plot + geom_line(mapping=aes(size, real_time, color=name), data=data[data$name=="robin_hood_median",], size=2)
-plot = plot + ggtitle(label="unordered_map<size_t, std::vector<uint32_t>>::at performance", subtitle = "runtime over size of the map, sampled at n*50000\nqueries randomized each call, 20 benchmark cycles per map size")
+plot = plot + ggtitle(label="unordered_map::at performance", subtitle = "runtime over size of the map, sampled at n*50000\nqueries randomized each call, 20 benchmark cycles per map size")
 plot = plot + xlab("# entries in map") + scale_y_continuous(name="runtime (ns)", breaks=seq(0, 50000, 50)) + xlim(c(0, 1.5*1e+06))
 plot = plot + theme(plot.title=element_text(face="bold", size=30), plot.subtitle=element_text(size=12), axis.title=element_text(size=20), axis.text=element_text(size=12), legend.title=element_text(size=20), legend.text=element_text(size=20))
 labels = c("absl::", "boost::", "robin_hood::", "std::")
